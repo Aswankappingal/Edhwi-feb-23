6f6c81915ed6d8695830feb2e9aa423a22ba776e
Aswankappingal <aswankappingal@gmail.com>
2026-03-04 11:46:09 +0530

    pushed

--- src/components/Pages/Gallery/Gallery.jsx
+++ src/components/Pages/Gallery/Gallery.jsx
@@ -4,14 +4,14 @@ import { FaPlay } from 'react-icons/fa';
 
 const Gallery = () => {
     const images = [
-        { id: 7, src: "/gallery7.svg", alt: "Gallery Video 1", isVideo: true },
-        { id: 8, src: "/galley8.svg", alt: "Gallery Video 2", isVideo: true },
-        { id: 1, src: "/gallery1.svg", alt: "Gallery Image 1" },
-        { id: 2, src: "/gallery2.svg", alt: "Gallery Image 2" },
-        { id: 3, src: "/gallery3.svg", alt: "Gallery Image 3" },
-        { id: 4, src: "/gallery4.svg", alt: "Gallery Image 4" },
-        { id: 5, src: "/gallery5.svg", alt: "Gallery Image 5" },
-        { id: 6, src: "/gallery6.svg", alt: "Gallery Image 6" },
+        { id: 1, src: "/gallery7.svg", alt: "Gallery Video 1", isVideo: true },
+        { id: 2, src: "/galley8.svg", alt: "Gallery Video 2", isVideo: true },
+        { id: 3, src: "/gallery1.svg", alt: "Gallery Image 1" },
+        { id: 4, src: "/gallery2.svg", alt: "Gallery Image 2" },
+        { id: 5, src: "/gallery3.svg", alt: "Gallery Image 3" },
+        { id: 6, src: "/gallery4.svg", alt: "Gallery Image 4" },
+        { id: 7, src: "/gallery5.svg", alt: "Gallery Image 5" },
+        { id: 8, src: "/gallery6.svg", alt: "Gallery Image 6" },
 
     ];
 
